1b7daa4c62d03998d0fa249c82f342749edd3d64
deshen24 <deshen@mit.edu>
2017-08-24 22:39:50 -0400

    with california case study

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ title = "Basque country study"
 case_study(basque, region, year, num_sv=2, method="Bayes")
 
 # CALIFORNIA PROP 99 STUDY
-"""df = pd.read_csv('cali.csv')
+df = pd.read_csv('cali.csv')
 df = df[df['SubMeasureDesc'] == 'Cigarette Consumption (Pack Sales Per Capita)']
 cali = df.pivot_table(values='Data_Value', index='LocationDesc', columns='Year')
-drop_list = [1, 2, 8, 9, 11, 20, 21, 22, 30, 32, 37, 47]
+#drop_list = [1, 2, 8, 9, 11, 20, 21, 22, 30, 32, 37, 47]
 #data = data.drop(data.index[drop_list])
 region = "California"
 year = 18
@@ -37,4 +37,4 @@ year_shift = 1970
 xlabel = "year"
 ylabel = "per-capita cigarette sales (in packs)"
 title = "Tobacco case study"
-case_study(cali, region, year, num_sv=2)"""
+case_study(cali, region, year, num_sv=2, method="Bayes")
